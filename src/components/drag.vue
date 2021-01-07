8ddd076bc011c823f0e8a682bde1c24b52ab34e2
Yuta Miyasaka <miyaska_y@cyberowl.co.jp>
2021-01-07 22:15:06 +0900

    [fixed]table drag select

--- src/components/drag.vue
+++ src/components/drag.vue
@@ -32,7 +32,8 @@
 </template>
 
 <script>
-import {scanTable} from "./scantable"
+import { scanTable, dragSelectedCells } from "./scantable"
+// import { scanTable } from "./scantable"
 
 export default {
   data(){
@@ -43,25 +44,6 @@ export default {
       endCell:[],
       isDrag: false,
       rows: [
-        {
-          "table_cells": [
-            {
-              "cell_type": "TD",
-              "rowspan": 1,
-              "colspan": 3,
-            },
-            {
-              "cell_type": "TD",
-              "rowspan": 1,
-              "colspan": 1,
-            },
-            {
-              "cell_type": "TD",
-              "rowspan": 1,
-              "colspan": 3,
-            }
-          ]
-        },
         {
           "table_cells": [
             {
@@ -78,6 +60,25 @@ export default {
               "cell_type": "TD",
               "rowspan": 1,
               "colspan": 2,
+            }
+          ]
+        },
+        {
+          "table_cells": [
+            {
+              "cell_type": "TD",
+              "rowspan": 2,
+              "colspan": 1,
+            },
+            {
+              "cell_type": "TD",
+              "rowspan": 1,
+              "colspan": 2,
+            },
+            {
+              "cell_type": "TD",
+              "rowspan": 1,
+              "colspan": 1,
             }
           ]
         },
@@ -148,6 +149,7 @@ export default {
     clear(){
       this.currentCells = []
     },
+    //ドラッグ選択
     mouseDown(e){
       console.log("mouseDown:",e)
       this.isDrag = true
@@ -172,28 +174,29 @@ export default {
           rowspan: e.target.rowSpan,
           colspan: e.target.colSpan
         } 
+        console.log("endCell:",this.endCell)
       }
     }
   },
   //dragによる選択範囲の算出
   computed:{
-    colIndicies(){
+    colIndices(){
       return scanTable( this.rows )
     }
   },
   watch:{
     endCell(){
-      this.currentCells = [this.startCell]
+      this.currentCells = []
 
-      // 基点となるrowとcol (scanTableから算出　※結合状況でズレる)
+      // 基点となるrowとcol
       let startRowIndex = this.startCell.rowIndex
       if ( startRowIndex > this.endCell.rowIndex ){
         startRowIndex = this.endCell.rowIndex
       }
 
-      let startColIndex = this.colIndicies[this.startCell.rowIndex][this.startCell.cellIndex]
-      if ( startColIndex > this.colIndicies[this.endCell.rowIndex][this.endCell.cellIndex] ){
-        startColIndex = this.colIndicies[this.endCell.rowIndex][this.endCell.cellIndex]
+      let startColIndex = this.colIndices[this.startCell.rowIndex][this.startCell.cellIndex]
+      if ( startColIndex > this.colIndices[this.endCell.rowIndex][this.endCell.cellIndex] ){
+        startColIndex = this.colIndices[this.endCell.rowIndex][this.endCell.cellIndex]
       }
 
       let endRowIndex = this.startCell.rowIndex + this.startCell.rowspan - 1 
@@ -201,44 +204,71 @@ export default {
         endRowIndex = this.endCell.rowIndex + this.endCell.rowspan - 1
       }
 
-      let endColIndex = this.colIndicies[this.startCell.rowIndex][this.startCell.cellIndex] + this.startCell.colspan - 1 
-      if ( endColIndex < this.colIndicies[this.endCell.rowIndex][this.endCell.cellIndex] + this.endCell.colspan - 1 ){
-        endColIndex = this.colIndicies[this.endCell.rowIndex][this.endCell.cellIndex] + this.endCell.colspan - 1
+      let endColIndex = this.colIndices[this.startCell.rowIndex][this.startCell.cellIndex] + this.startCell.colspan - 1 
+      if ( endColIndex < this.colIndices[this.endCell.rowIndex][this.endCell.cellIndex] + this.endCell.colspan - 1 ){
+        endColIndex = this.colIndices[this.endCell.rowIndex][this.endCell.cellIndex] + this.endCell.colspan - 1
       }
-
-      console.log("endColIndex:", endColIndex)
       
-      const rowWidth = endRowIndex - startRowIndex + 1
-      const colWidth = endColIndex - startColIndex + 1
-
-      console.log("endRowColWidth:", rowWidth, colWidth)
-
       //対象セルの抽出
-      const dragSelectedCells = () => {
-        for( let i = startRowIndex; i <= endRowIndex; i++ ){
-          for( let j = 0; j < this.rows[i].table_cells.length; j++ ){
-            if( this.colIndicies[i][j] >= startColIndex && this.colIndicies[i][j] <= endColIndex ){
-              //範囲拡張チェック
-              if( i + this.rows[i].table_cells[j].rowspan - 1 > endRowIndex ){
-                endRowIndex = this.rows[i].table_cells[j].rowspan
-                dragSelectedCells()
-              }
+      // const dragSelectedCells = () => {
+      //   for( let i = 0; i <= endRowIndex; i++ ){
+      //     for( let j = 0; j < this.rows[i].table_cells.length; j++ ){            
+      //       //範囲拡張チェック
+      //       //colIndexの延長処理
+      //       if( i <= startRowIndex && startRowIndex <= i + this.rows[i].table_cells[j].rowspan - 1
+      //           || i <= startRowIndex && startRowIndex <= i + this.rows[i].table_cells[j].rowspan - 1 
+      //           || startRowIndex <= i && i + this.rows[i].table_cells[j].rowspan - 1 <= endRowIndex){
+      //             if( this.colIndices[i][j] < startColIndex &&  startColIndex <= this.colIndices[i][j] + this.rows[i].table_cells[j].colspan - 1 ){
+      //               startColIndex = this.colIndices[i][j]
+      //               dragSelectedCells()
+      //             }
 
-              if( this.colIndicies[i][j] + this.rows[i].table_cells[j].colspan - 1 > endColIndex ){
-                endColIndex = this.colIndicies[i][j] + this.rows[i].table_cells[j].colspan - 1
-                dragSelectedCells()
-              }
+      //             if( this.colIndices[i][j] <= endColIndex &&  endColIndex < this.colIndices[i][j] + this.rows[i].table_cells[j].colspan - 1 ){
+      //               endColIndex = this.colIndices[i][j] + this.rows[i].table_cells[j].colspan - 1
+      //               dragSelectedCells()
+      //             }
+      //       }
 
-              this.currentCells.push({
-                rowIndex: i,
-                cellIndex: j,
-              })
-            }
-          }
-        }
-      }
+      //       //rowIndexの延長処理
+      //       if( this.colIndices[i][j] <= startColIndex && startColIndex <= this.colIndices[i][j] + this.rows[i].table_cells[j].colspan - 1
+      //           || this.colIndices[i][j] <= endColIndex && endColIndex <= this.colIndices[i][j] + this.rows[i].table_cells[j].colspan - 1 
+      //           || startColIndex <= this.colIndices[i][j] && this.colIndices[i][j] + this.rows[i].table_cells[j].colspan - 1 <= endColIndex ){
+      //             if( i < startRowIndex && startRowIndex <= (i + this.rows[i].table_cells[j].rowspan - 1) ){
+      //               startRowIndex = i
+      //               dragSelectedCells()
+      //             }
 
-      dragSelectedCells()
+      //             if( i <= endRowIndex && endRowIndex < (i + this.rows[i].table_cells[j].rowspan - 1) ){
+      //               endRowIndex = i + this.rows[i].table_cells[j].rowspan - 1
+      //               dragSelectedCells()
+      //             }
+      //       }
+            
+            
+      //       if( startRowIndex <= i && i <= endRowIndex
+      //           && startColIndex <= this.colIndices[i][j] && this.colIndices[i][j] <= endColIndex ){
+      //             this.currentCells.push({
+      //               rowIndex: i,
+      //               cellIndex: j,
+      //               colIndex: this.colIndices[i][j],
+      //               rowspan: this.rows[i].table_cells[j].rowspan,
+      //               colspan: this.rows[i].table_cells[j].colspan,
+      //             })
+      //       }
+      //     }
+      //   }
+      // }
+      // dragSelectedCells()
+
+      this.currentCells = dragSelectedCells({
+        startRowIndex: startRowIndex,
+        endRowIndex: endRowIndex,
+        startColIndex: startColIndex,
+        endColIndex: endColIndex,
+        rows: this.rows,
+        colIndices: this.colIndices,
+        currentCells: this.currentCells,
+      })
     }
   }
 }
--- src/components/scantable.js
+++ src/components/scantable.js
@@ -1,31 +1,122 @@
 const scanTable = (rows) => 
+{
+  const arr = [];
+  const colIndices = []
+  for(var y=0; y < rows.length; y++)
   {
-      const arr = [];
-      const colIndices = []
-      for(var y=0; y < rows.length; y++)
+      var row = rows[y]
+  
+      for(var x=0;x<row.table_cells.length;x++)
       {
-          var row = rows[y]
-      
-          for(var x=0;x<row.table_cells.length;x++)
-          {
-              var cell = row.table_cells[x], xx = x, tx, ty;
-              
-              for(;arr[y] && arr[y][xx]; ++xx);
+          var cell = row.table_cells[x], xx = x, tx, ty;
+          
+          for(;arr[y] && arr[y][xx]; ++xx);
 
-              for(tx = xx; tx < xx + (cell.colspan || 1); ++tx) {
-                  for(ty = y; ty < y + (cell.rowspan || 1); ++ty)
-                  {
-                      if( !arr[ty] ) arr[ty] = []          
-                      arr[ty][tx] = 1
-                      
-                      if ( !colIndices[y] ) colIndices[y] = []
-                      colIndices[y][x] = xx
-                  }
+          for(tx = xx; tx < xx + (cell.colspan || 1); ++tx) {
+              for(ty = y; ty < y + (cell.rowspan || 1); ++ty)
+              {
+                  if( !arr[ty] ) arr[ty] = []          
+                  arr[ty][tx] = 1
+                  
+                  if ( !colIndices[y] ) colIndices[y] = []
+                  colIndices[y][x] = xx
               }
-
           }
-      }
-      return colIndices
-  }
 
-export { scanTable }
+      }
+  }
+  return colIndices
+}
+
+
+const dragSelectedCells = ({
+  startRowIndex,
+  endRowIndex,
+  startColIndex,
+  endColIndex,
+  rows,
+  colIndices,
+  currentCells,
+}) => {
+  for( let i = 0; i <= endRowIndex; i++ ){
+    for( let j = 0; j < rows[i].table_cells.length; j++ ){            
+      //範囲拡張チェック
+      //colIndexの延長処理
+      if( i <= startRowIndex && startRowIndex <= i + rows[i].table_cells[j].rowspan - 1
+          || i <= startRowIndex && startRowIndex <= i + rows[i].table_cells[j].rowspan - 1 
+          || startRowIndex <= i && i + rows[i].table_cells[j].rowspan - 1 <= endRowIndex){
+            if( colIndices[i][j] < startColIndex &&  startColIndex <= colIndices[i][j] + rows[i].table_cells[j].colspan - 1 ){
+              startColIndex = colIndices[i][j]
+              dragSelectedCells({
+                startRowIndex,
+                endRowIndex,
+                startColIndex,
+                endColIndex,
+                rows,
+                colIndices,
+                currentCells,
+              })
+            }
+
+            if( colIndices[i][j] <= endColIndex &&  endColIndex < colIndices[i][j] + rows[i].table_cells[j].colspan - 1 ){
+              endColIndex = colIndices[i][j] + rows[i].table_cells[j].colspan - 1
+              dragSelectedCells({
+                startRowIndex,
+                endRowIndex,
+                startColIndex,
+                endColIndex,
+                rows,
+                colIndices,
+                currentCells,
+              })
+            }
+      }
+
+      //rowIndexの延長処理
+      if( colIndices[i][j] <= startColIndex && startColIndex <= colIndices[i][j] + rows[i].table_cells[j].colspan - 1
+          || colIndices[i][j] <= endColIndex && endColIndex <= colIndices[i][j] + rows[i].table_cells[j].colspan - 1 
+          || startColIndex <= colIndices[i][j] && colIndices[i][j] + rows[i].table_cells[j].colspan - 1 <= endColIndex ){
+            if( i < startRowIndex && startRowIndex <= (i + rows[i].table_cells[j].rowspan - 1) ){
+              startRowIndex = i
+              dragSelectedCells({
+                startRowIndex,
+                endRowIndex,
+                startColIndex,
+                endColIndex,
+                rows,
+                colIndices,
+                currentCells,
+              })
+            }
+
+            if( i <= endRowIndex && endRowIndex < (i + rows[i].table_cells[j].rowspan - 1) ){
+              endRowIndex = i + rows[i].table_cells[j].rowspan - 1
+              dragSelectedCells({
+                startRowIndex,
+                endRowIndex,
+                startColIndex,
+                endColIndex,
+                rows,
+                colIndices,
+                currentCells,
+              })
+            }
+      }
+      
+      
+      if( startRowIndex <= i && i <= endRowIndex
+          && startColIndex <= colIndices[i][j] && colIndices[i][j] <= endColIndex ){
+            currentCells.push({
+              rowIndex: i,
+              cellIndex: j,
+              colIndex: colIndices[i][j],
+              rowspan: rows[i].table_cells[j].rowspan,
+              colspan: rows[i].table_cells[j].colspan,
+            })
+      }
+    }
+  }
+  return currentCells
+}
+
+export { scanTable, dragSelectedCells }
